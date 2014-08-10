8378b4539e5094765f89226cc5717fe0db89f869
takako <takakoiwamatsu.sc@gmail.com>
2014-08-10 09:20:14 +0900

    Improve the README file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+SampleApp02::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
