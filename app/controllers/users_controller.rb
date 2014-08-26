add3dd42f7f74fc412cba3c7f2037a8bc1ac38a7
takako <takakoiwamatsu.sc@gmail.com>
2014-08-27 00:27:44 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
 
-　def index
-    @users = User.paginate(page: params[:page])
-  end　
+  def index
+    @users = User.all
+    #@users = User.paginate(page: params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
-   def create
-    @user = User.new(user_params)    
+  def create
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
@@ -28,4 +29,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-end
+  end
